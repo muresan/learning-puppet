7a2552f5605ff28740eae03eac00f57660989ad4
Catalin Muresan <muresan@pythian.com>
2013-03-12 18:05:02 +0200

    Some more work on the stored configs bind module

--- bind/manifests/hostentry.pp
+++ bind/manifests/hostentry.pp
@@ -1,3 +1,13 @@
+#class dns::hostentry {
+#  @@hostentry { "${::fqdn}":
+#    host_name   => $::hostname,
+#    domain_name => $::domainname,
+#    fqdn        => $::fqdn,
+#    ip_address  => $::ipaddress,
+#    zone        => $::ec2_placement_availability_zone
+#    provider    => "aws",
+#  }
+
 define bind::hostentry (
   $host_name,
   $domain_name,
@@ -6,20 +16,24 @@ define bind::hostentry (
   $order  = '20',
   $zone,
   $provider,
+  $domain,
 ) {
-  concat::fragment { "${host_name}.${zone}.${provider}":
+  concat::fragment { "${host_name}.${zone}.${provider}.${domain}":
     order   => 20,
-    target  => '/var/named/',
+    target  => "${chroot}/var/named/${domain}",
     content => template('puppet:///modules/bind/hostentry.erb'),
   }
-  #concat::fragment { "${host_name}.servers":
+  
+  ## added both entries at the same time in $domain zone
+  #concat::fragment { "${host_name}.servers.${domain}":
   #  order   => 20,
-  #  target  => '/var/named/',
+  #  target  => "${chroot}/var/named/servers.${domain}",
   #  content => template('puppet:///modules/bind/hostentry.erb'),
   #}
-  concat::fragment { "${host_name}.servers":
+  
+  concat::fragment { "${host_name}.10.in-addr.arpa":
     order   => 20,
-    target  => '/var/named/',
+    target  => "${chroot}/var/named/10.in-addr.arpa",
     content => template('puppet:///modules/bind/reverseentry.erb'),
   }
 }
--- bind/manifests/init.pp
+++ bind/manifests/init.pp
@@ -13,52 +13,94 @@
 class bind (
   $version    = "installed",
   $start_bind = true,
-  $chroot     = true,
+  $chroot     = "/var/named/chroot",
+  $domain     = "tinyco.com",
 ) {
   
   $utime_serial = inline_template("<%= Time.now.to_i %>")
+
   $bind_package = $::operatingsystem ? {
-      "CentOS" => 'bind',
-      "RedHat" => 'bind',
-      "Ubuntu" => 'bind9',
-      default =>  'bind',
-    }
-  
+    "CentOS" => 'bind',
+    "RedHat" => 'bind',
+    "Ubuntu" => 'bind9',
+    default =>  'bind',
+  }
+
+  $bind_service = $::operatingsystem ? {
+    "CentOS" => 'named',
+    "RedHat" => 'named',
+    "Ubuntu" => 'bind9',
+    default =>  'named',
+  }
   
   package { $bind_package: 
     ensure  => $version,
   }
+  
+  service { $bind_service:
+    ensure    => running,
+    subscribe => [
+      File["named.conf"],Concat["${chroot}/var/named/${domain}","${chroot}/var/named/10.in-addr.arpa"],
+    ],
+    require   => Package[$bind_package],
+  }
 
-  file { "/etc/named.conf":
+  file { "named.conf":
     owner   => root,
     group   => named,
     mode    => 0640,
+    path    => "/etc/named.conf",
     content => template("named/named.conf.erb"),
     require => Package[$bind_package],
   }
-
-  file { "/var/named/chroot/var/named/$domain.hosts":
-    owner   => root,
-    group   => named,
-    mode    => 0640,
-    content => template("named/domain.hosts.erb"),
-    require => Package[$bind_package],
+  
+  concat::fragment { "header.${domain}":
+    target  => "${chroot}/var/named/${domain}",
+    order   => '01',
+    content => ";; This file managed by Puppet\n",
   }
-
-  class dns::host {
-    @@hostentry { "${::fqdn}":
-      host_name   => $::hostname,
-      domain_name => $::domainname,
-      fqdn        => $::fqdn,
-      ip_address  => $::ipaddress,
-    }
-
-    class dns::ips {
-      Hostentry <<| |>> {
-        notify => Service["named"]
-      }
-    }
-
+  
+  concat::fragment { "soa.${domain}":
+    target  => "${chroot}/var/named/${domain}",
+    order   => '10',
+    content => template('puppet:///modules/bind/templates/soa.erb'),
   }
+  
+  concat::fragment { "header.10.in-addr.arpa":
+    target  => "${chroot}/var/named/10.in-addr.arpa",
+    order   => '01',
+    content => ";; This file managed by Puppet\n",
+  }
+  
+  concat::fragment { "soa.10.in-addr.arpa":
+    target  => "${chroot}/var/named/10.in-addr.arpa",
+    order   => '10',
+    content => template('puppet:///modules/bind/templates/soa.erb'),
+  }
+  
+  Bind::Hostentry <<| |>>
 
+  #file { "$domain":
+  #  owner   => root,
+  #  group   => named,
+  #  mode    => 0640,
+  #  path    => "${chroot}/var/named/${domain}",
+  #  content => template("puppet:///modules/bind/hostentry.erb"),
+  #  require => Package[$bind_package],
+  #}
+  
+  #file { "10.in-addr.arpa":
+  #  owner   => "root",
+  #  group   => "named",
+  #  mode    => 640,
+  #  path    => "${chroot}/var/named/10.in-addr.arpa",
+  #  content => template("puppet:///modules/bind/reverseentry.erb")
+  #}
+
+  #  class dns::ips {
+  #    Hostentry <<| |>> {
+  #      notify => Service["named"]
+  #    }
+  #  }
+  
 }
