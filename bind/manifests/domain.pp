541fba750296f1968f475670ea2d303c446bf490
Catalin Muresan <muresan@pythian.com>
2013-03-13 16:28:45 +0200

    Multiple changes: added bind::domain to support multiple domains.

---
+++ bind/manifests/domain.pp
@@ -0,0 +1,54 @@
+# Class: bind::domain
+#
+# This module manages bind domains
+#
+# Parameters: reverse true|false if the domain is a reverse domain, so we will add the $ORIGIN entry
+#
+# Actions: creates the /var/named/$name domain entries
+#
+# Requires: see Modulefile
+#
+# Sample Usage:
+#
+class bind::domain (  
+  $reverse = false 
+) {
+
+  concat { "/var/named/${name}":
+    owner   => root,
+    group   => named,
+    mode    => '0644',
+    require => Package[$bind::bind_package],
+    notify  => Service[$bind::bind_service],
+  }
+
+  concat::fragment { "header.${name}":
+    target  => "/var/named/${name}",
+    order   => 10,
+    content => ";; This file managed by Puppet\n",
+  }
+
+  concat::fragment { "soa.${name}":
+    target  => "/var/named/${name}",
+    order   => 20,
+    content => template('bind/soa.erb'),
+  }
+
+  if ( $reverse == true ){
+	  concat::fragment { "origin.${name}":
+	    target  => "/var/named/${name}",
+	    order   => 30,
+	    content => "\$ORIGIN in-addr.arpa.\n",
+	  }
+  }
+
+  concat::fragment { "named.conf.${name}":
+    target  => "/etc/named.conf",
+    order   => 20,
+    content => template("bind/zone.named.conf.erb"), 
+  }
+
+  Bind::Hostentry <<| domain_name == $name |>>
+
+}
+
--- bind/manifests/init.pp
+++ bind/manifests/init.pp
@@ -13,7 +13,7 @@
 class bind (
   $version     = "installed",
   $start_bind  = true,
-  $domain_name = "tinyco.com"
+  $domain_name = "$::domain"
 ) {
   include concat::setup
 
@@ -37,87 +37,30 @@ class bind (
 
   service { $bind_service:
     ensure    => running,
-    subscribe => [File["named.conf"], Concat["/var/named/${domain_name}", "/var/named/10.in-addr.arpa"],],
+    subscribe => File["named.conf"],
     require   => Package[$bind_package],
   }
 
-  file { "named.conf":
-    owner   => root,
-    group   => named,
-    mode    => '0640',
-    path    => "/etc/named.conf",
-    content => template('bind/named.conf.erb'),
-    require => Package[$bind_package],
-  }
-
-  concat { "/var/named/${domain_name}":
-    owner   => root,
-    group   => named,
-    mode    => '0644',
-    require => Package[$bind_package],
-    notify  => Service[$bind_service],
-  }
-
-  concat { "/var/named/10.in-addr.arpa":
-    owner   => root,
-    group   => named,
-    mode    => '0644',
-    require => Package[$bind_package],
-    notify  => Service[$bind_service],
-  }
-
-  concat::fragment { "header.${domain_name}":
-    target  => "/var/named/${domain_name}",
-    order   => 10,
-    content => ";; This file managed by Puppet\n",
-  }
-
-  concat::fragment { "soa.${domain_name}":
-    target  => "/var/named/${domain_name}",
-    order   => 20,
-    content => template('bind/soa.erb'),
-  }
-
-  concat::fragment { "header.10.in-addr.arpa":
-    target  => "/var/named/10.in-addr.arpa",
-    order   => 10,
-    content => ";; This file managed by Puppet\n",
-  }
-
-    concat::fragment { "soa.10.in-addr.arpa":
-    target  => "/var/named/10.in-addr.arpa",
-    order   => 20,
-    content => template('bind/soa.erb'),
-  }
-
-  concat::fragment { "origin.10.in-addr.arpa":
-    target  => "/var/named/10.in-addr.arpa",
-    order   => 30,
-    content => "\$ORIGIN in-addr.arpa.\n",
-  }
-
-  Bind::Hostentry <<| |>>
-
-  # file { "$domain_name":
+  #file { "named.conf":
   #  owner   => root,
   #  group   => named,
-  #  mode    => 0640,
-  #  path    => "/var/named/${domain_name}",
-  #  content => template("puppet:///modules/bind/hostentry.erb"),
+  #  mode    => '0640',
+  #  path    => "/etc/named.conf",
+  #  content => template('bind/named.conf.erb'),
   #  require => Package[$bind_package],
   #}
+  
+  concat { "/etc/named.conf":
+    owner   => root,
+    group   => named,
+    mode    => '0644',
+    require => Package[$bind_package],
+    notify  => Service[$bind_service],
+  }
 
-  # file { "10.in-addr.arpa":
-  #  owner   => "root",
-  #  group   => "named",
-  #  mode    => 640,
-  #  path    => "/var/named/10.in-addr.arpa",
-  #  content => template("puppet:///modules/bind/reverseentry.erb")
-  #}
+  concat::fragment { "header.named.conf":
+    order   => 10,
+    content => template('bind/named.conf.erb'),
+  }
 
-  #  class dns::ips {
-  #    Hostentry <<| |>> {
-  #      notify => Service["named"]
-  #    }
-  #  }
 }
